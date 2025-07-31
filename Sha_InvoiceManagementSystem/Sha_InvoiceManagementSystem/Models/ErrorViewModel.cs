726ae55a4a1ccb14dfabe6c0bcb40ca0bb74ad98
fatma elshihna <elshinafatma@gmail.com>
2025-07-31 23:26:57 +0300

    Created project and added models from database

---
+++ Sha_InvoiceManagementSystem/Sha_InvoiceManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sha_InvoiceManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
